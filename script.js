be03ba3c7385e6f67a1233061235c812690d5f02
EligioBiasi <biasi.eligio@edufaimm.it>
2023-05-12 15:52:28 +0200

    discount added

--- script.js
+++ script.js
@@ -3,10 +3,21 @@ const kilometers = parseInt (prompt('inserisci i km da percorrere'));
 const age = parseInt (prompt('inserisci la tua età'));
 console.log(kilometers);
 console.log(age);
+
 // il prezzo del biglietto è definito in base ai km (0.21 € al km)
 let ticketCost = (kilometers * 0.21);
+let discount;
+
 // va applicato uno sconto del 20% per i minorenni
-if (age<18){
-    ticketCost = ((20 * ticketCost) / 100);
-}
 // va applicato uno sconto del 40% per gli over 65.
+if (age<18){
+    discount = ((20 * ticketCost) / 100);
+} else if (age>65){
+    discount = ((40 * ticketCost) / 100);
+} else ((age>=18) && (age<=65));{
+    discount = 0
+};
+
+const ticketCostFinal = ticketCost - discount;
+
+document.getElementById('cost').innerHTML+=ticketCostFinal + '€';
